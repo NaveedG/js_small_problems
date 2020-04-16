d11de09f348f01873c48585bc2591c4afd1ab7c9
NaveedG <nganjani@protonmail.com>
2020-04-16 16:22:34 -0500

    easy 1 ex. 2 completed

---
+++ easy_1/02_odd_numbers.js
@@ -0,0 +1,62 @@
+// Log all odd numbers from 1 to 99, inclusive, to the console. Log all numbers on separate lines.
+
+oddNumbers = () => {
+  let x = 1
+  while (x <= 99) {
+    console.log(x)
+    x += 2
+  }
+}
+
+oddNumbers() /*
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+81
+83
+85
+87
+89
+91
+93
+95
+97
+99 
+*/
