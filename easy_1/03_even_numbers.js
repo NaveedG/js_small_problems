b14a97e661e214a1b67341ddf6a239969dc33d34
NaveedG <nganjani@protonmail.com>
2020-04-16 16:30:26 -0500

    easy 1 ex. 2 and 3 completed

---
+++ easy_1/03_even_numbers.js
@@ -0,0 +1,9 @@
+// Log all even numbers from 1 to 99, inclusive, to the console. Log all numbers on separate lines.
+
+evenNumbers = () => {
+  let x = 2
+  while (x <= 99) {
+    console.log(x)
+    x += 2
+  }
+}
