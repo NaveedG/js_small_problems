7a5a926da5d74a0f45b502b616e4f3a95d1932e3
NaveedG <nganjani@protonmail.com>
2020-04-17 11:34:18 -0500

    easy 4 ex. 4 completed

---
+++ easy_1/04_how_big_is_the_room.js
@@ -0,0 +1,15 @@
+// Build a program that asks the user to enter the length and width of a room in meters, and then logs the area of the room to the console in both square meters and square feet.
+
+let readlineSync = require('readline-sync');
+
+// retrieve length input from user
+console.log('Enter the length of the room in meters:')
+let length = readlineSync.prompt()
+
+// retrieve width input from user
+console.log('Enter the width of the room in meters:')
+let width = readlineSync.prompt()
+
+// log area of the room in square meters and square feet
+let area = length * width
+console.log(`The area of the room is ${area.toFixed(2)} square meters (${(area * 10.7639).toFixed(2)} square feet).`)
