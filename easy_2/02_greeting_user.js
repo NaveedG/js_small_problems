3379064789ef4541979ee43a1c94da5a50872963
NaveedG <nganjani@protonmail.com>
2020-04-22 12:40:20 -0500

    easy 2 ex. 5 completed/ex. 2 updated

--- easy_2/02_greeting_user.js
+++ easy_2/02_greeting_user.js
@@ -1,21 +1,21 @@
 /* Write a program that will ask for a user's name. The program will then greet the user. 
-If the user writes "name!" then the computer yells back to the user. */
+If the user writes "name!" then the computer yells back to the user.
 
-let readlineSync = require('readline-sync')
+Examples:
+What is your name? Bob
+Hello Bob.
+
+What is your name? Bob!
+HELLO BOB. WHY ARE WE SCREAMING? */
+
+const readlineSync = require('readline-sync')
 
 // ask for user's name
 console.log('What is your name?')
-let name = readlineSync.prompt()
+const name = readlineSync.prompt()
 
 // greet user in caps unless name lacks !
   if (name.charAt(name.length - 1) === '!')
     console.log(`HELLO ${name.slice(0, -1).toUpperCase()}. WHY ARE YOU SCREAMING?`)
   else
     console.log(`Hello ${name}.`)
-
-/* Examples
-What is your name? Bob
-Hello Bob.
-
-What is your name? Bob!
-HELLO BOB. WHY ARE WE SCREAMING? */
