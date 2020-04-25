5f8cb0d1169cd0a0f6460fcfa3b8e2aa2d4ac605
NaveedG <nganjani@protonmail.com>
2020-04-24 19:24:21 -0500

    easy 2 ex. 11 completed

---
+++ easy_2/11_convert_number_to_string.js
@@ -0,0 +1,25 @@
+/* Write a function that converts a non-negative integer value (e.g., 0, 1, 2, 3, and so on) to the string representation of that integer.
+
+You may not use any of the standard conversion functions available in JavaScript, such as String(), Number.prototype.toString, or an expression such as '' + number. 
+Your function should do this the old-fashioned way and construct the string by analyzing and manipulating the number. */
+
+integerToString = num => {
+  let obj = { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9' }
+  let str = ''
+  if (num === 0)
+    str = obj[num]
+  else {
+    let ones
+    while (num > 0) {
+      ones = num % 10
+      str = obj[ones] + str
+      num = Math.floor(num / 10)
+    }
+  }
+  return str
+}
+
+console.log(integerToString(4321)); // "4321"
+console.log(integerToString(0)); // "0"
+console.log(integerToString(5000)); // "5000"
+console.log(integerToString(1234567890)); // "1234567890"
