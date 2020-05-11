372e74970a9fc25ff6f679bee2d221e36c506595
NaveedG <nganjani@protonmail.com>
2020-05-11 17:12:27 -0500

    easy 5 ex. 11 completed

---
+++ easy_5/11_after_midnight.js
@@ -0,0 +1,27 @@
+/* The time of day can be represented as the number of minutes before or after midnight. 
+If the number of minutes is positive, the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+Write a function that takes a time using this minute-based format and returns the time of day in 24 hour format (hh:mm). Your method should work with any integer input.
+
+You may not use javascript's Date class methods. */
+
+timeOfDay = num => {
+  while (num > 1440) num -= 1440 // if number exceeds 24 hours
+  while (num < 0) num += 1440 // if number is negative
+
+  let hh = Math.floor(num / 60)
+  let mm = num % 60
+
+  if (hh < 10) hh = '0' + String(hh)
+  if (mm < 10) mm = '0' + String(mm)
+
+  return `${hh}:${mm}`
+}
+
+console.log(timeOfDay(0)); // "00:00"
+console.log(timeOfDay(-3)); // "23:57"
+console.log(timeOfDay(35)); // "00:35"
+console.log(timeOfDay(-1437)); // "00:03"
+console.log(timeOfDay(3000)); // "02:00"
+console.log(timeOfDay(800)); // "13:20"
+console.log(timeOfDay(-4231)); // "01:29"
