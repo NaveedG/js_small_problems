fc9421e1da1ca2cd916b4f71d92d6244bee01d69
NaveedG <nganjani@protonmail.com>
2020-05-14 17:07:08 -0500

    easy 6 ex. 11 completed

---
+++ easy_6/11_reversed_arrays.js
@@ -0,0 +1,35 @@
+/* Write a function that takes an Array as an argument, and reverses its elements in place; that is, mutate the Array passed into this method. 
+The return value should be the same Array object.
+
+You may not use Array.prototype.reverse(). */
+
+reverse = (arr, count = 0) => {
+  if (count === Math.floor(arr.length / 2))
+    return arr
+  else {
+    let temp = arr[count]
+    arr[count] = arr[arr.length - count - 1]
+    arr[arr.length - count - 1] = temp
+    return reverse(arr, count += 1)
+  }
+}
+
+let list = [1, 2, 3, 4];
+let result = reverse(list);
+console.log(result); // logs [4,3,2,1]
+console.log(list === result); // logs true
+
+let list1 = ["a", "b", "c", "d", "e"];
+let result1 = reverse(list1);
+console.log(result1); // logs  ["e", "d", "c", "b", "a"]
+console.log(list1 === result1); // logs true
+
+let list2 = ["abc"];
+let result2 = reverse(list2);
+console.log(result2); // logs  ["abc"]
+console.log(list2 === result2); // logs true
+
+let list3 = [];
+let result3 = reverse(list3);
+console.log(result3); // logs []
+console.log(list3 === result3); // logs true
