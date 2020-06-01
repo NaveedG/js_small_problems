a3fd632881efb9ea6af0a71b076b8852462433af
NaveedG <nganjani@protonmail.com>
2020-05-31 22:06:28 -0500

    medium 1 ex. 6 completed

---
+++ medium_1/06_recursive_fibonacci.js
@@ -0,0 +1,15 @@
+// Write a recursive function that computes the nth Fibonacci number, where nth is an argument passed to the function.
+
+fibonacci = num => {
+	if (num === 0) return 0
+	if (num === 1) return 1
+	return fibonacci(num - 1) + fibonacci(num - 2)
+}
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
