3d1d70bfe51691f43a5ec13869c1e9f6af456747
NaveedG <nganjani@protonmail.com>
2020-05-31 22:42:43 -0500

    medium 1 ex. 8 completed

---
+++ medium_1/08_fibonacci_memoization.js
@@ -0,0 +1,19 @@
+/* Memoization is an approach that involves saving a computed answer for future reuse, instead of computing it from scratch every time it is needed. 
+In the case of our recursive fibonacci function, using memoization saves calls to fibonacci(nth - 2) because the necessary values have already been computed by the recursive calls to fibonacci(nth - 1).
+
+For this exercise, your objective is to refactor the recursive fibonacci function to use memoization. */
+
+const obj = {}
+fibonacci = num => {
+	if (num === 0) return 0
+	if (num === 1) return 1
+	return (obj[num]) ? obj[num] : fibonacci(num - 1) + fibonacci(num - 2)
+}
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
