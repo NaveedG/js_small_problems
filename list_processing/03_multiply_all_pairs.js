4296896c4c8cd7e532833d29dab87f3afc952575
NaveedG <nganjani@protonmail.com>
2020-10-20 14:42:41 -0500

    list processing ex. 3 completed

--- list_processing/03_multiply_all_pairs.js
+++ list_processing/03_multiply_all_pairs.js
@@ -3,6 +3,10 @@ The returned array should be sorted in ascending numerical order.
 
 You may assume that neither argument will be an empty array. */
 
-
+multiplyAllPairs = (arr1, arr2) => {
+  const newArr = []
+  for (let num of arr1) arr2.forEach(val => newArr.push(num * val))
+  return newArr.sort((x, y) => x - y)
+}
 
 console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16]
