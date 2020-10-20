d253d0b84b870f2f40a3b8d4be62b943abc05a82
NaveedG <nganjani@protonmail.com>
2020-10-20 14:41:34 -0500

    list processing ex. 1/3 setup

--- list_processing/01_sum_of_digits.js
+++ list_processing/01_sum_of_digits.js
@@ -1,4 +1,5 @@
-// Write a function that takes one argument, a positive integer, and returns the sum of its digits. Do this without using for, while, or do...while loops - instead, use a series of method calls to perform the sum.
+/* Write a function that takes one argument, a positive integer, and returns the sum of its digits. 
+Do this without using for, while, or do...while loops - instead, use a series of method calls to perform the sum. */
 
 sum = num => {
   return num.toString().split('').map(n => parseInt(n)).reduce((sum, n) => sum + n)
