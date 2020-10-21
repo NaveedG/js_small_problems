5936a6ed118c45fafa7b44716295ea2743ba607c
NaveedG <nganjani@protonmail.com>
2020-10-21 15:10:19 -0500

    list processing ex. 7 completed

---
+++ list_processing/07_sum_of_sums.js
@@ -0,0 +1,16 @@
+/* Write a function that takes an array of numbers, and returns the sum of the sums of each leading subsequence for that array. 
+You may assume that the array always contains at least one number. */
+
+sumOfSums = arr => {
+  let sums = 0
+  while (arr.length > 0) {
+    sums += arr.reduce((sum, num) => sum + num)
+    arr.pop()
+  }
+  return sums
+}
+
+console.log(sumOfSums([3, 5, 2]));        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+console.log(sumOfSums([1, 5, 7, 3]));     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+console.log(sumOfSums([4]));              // 4
+console.log(sumOfSums([1, 2, 3, 4, 5]));  // 35
