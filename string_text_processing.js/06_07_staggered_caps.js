46791e060cc077eec493212613892fac38c21ab6
NaveedG <nganjani@protonmail.com>
2020-10-29 13:11:38 -0500

    string/text processing ex. 7 complete

--- string_text_processing.js/06_07_staggered_caps.js
+++ string_text_processing.js/06_07_staggered_caps.js
@@ -6,11 +6,7 @@ staggeredCase = str => {
   let count = 0
   const arr = str.split('').map(char => {
     count += 1
-    if (count % 2 != 0) {
-      return char.toUpperCase()
-    } else {
-      return char.toLowerCase()
-    }
+    return (count % 2 != 0) ? char.toUpperCase() : char.toLowerCase()
   })
   return arr.join('')
 }
@@ -18,3 +14,23 @@ staggeredCase = str => {
 console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
 console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
 console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
+
+/* Modify the function from the previous exercise so it ignores non-alphabetic characters when determining whether it should uppercase or lowercase each letter. 
+The non-alphabetic characters should still be included in the return value; they just don't count when toggling the desired case. */
+
+staggeredCase = str => {
+  let count = 0
+  const arr = str.split('').map(char => {
+    if (char === char.replace(/[a-z]/gmi)) {
+      return char
+    } else {
+      count += 1
+      return (count % 2 != 0) ? char.toUpperCase() : char.toLowerCase()
+    }
+  })
+  return arr.join('')
+}
+
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!"); // true
+console.log(staggeredCase("ALL CAPS") === "AlL cApS"); // true
+console.log(staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"); // true
